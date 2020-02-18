95350d9543ff6f078fce17e66a38ba4eb091915c
vagif4799 <quliyev_vaqif@icloud.com>
2020-02-18 22:16:27 +0400

    Initial Commit

---
+++ swift_road_map_step_0/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift_road_map_step_0
+//
+//  Created by Guliyev Vagif on 2/18/20.
+//  Copyright © 2020 Quliyev Vagif. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
